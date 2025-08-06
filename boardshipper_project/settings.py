273a594bf9c6ecabb57b05759b6a25bc38128f5b
martimlteixeira <martim@cymbiotika.com>
2025-08-05 20:41:50 -0700

    FIles

--- boardshipper_project/settings.py
+++ boardshipper_project/settings.py
@@ -114,6 +114,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
